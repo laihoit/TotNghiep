dd22412ad0aef5378eabd3c89f136af4dd86ea27
Lai Ho <hhnhan1@Nhans-iMac.local>
2018-08-14 10:09:56 +0700

    Done image picker

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image, Dimensions, Linking, Alert, TextInput } from 'react-native';
-
+import ImagePicker from 'react-native-image-picker';
 
 let rootNavigator = null;
+
+var options = {
+    title: 'Select Avatar',
+    customButtons: [
+      {name: 'fb', title: 'Choose Photo from Facebook'},
+    ],
+    storageOptions: {
+      skipBackup: true,
+      path: 'images'
+    }
+  };
 
 export function getRootNavigator() {
     return rootNavigator;
@@ -12,7 +23,9 @@ const { width, height } = Dimensions.get('window');
 class Home extends Component {
     constructor(props) {
         super(props);
-
+            this.state={
+                avatarSource:''
+            }
         rootNavigator = this.props.navigator;
         this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
     }
@@ -36,12 +49,36 @@ class Home extends Component {
 
     }
 
+    onImagePicker(){
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+          
+            if (response.didCancel) {
+              console.log('User cancelled image picker');
+            }
+            else if (response.error) {
+              console.log('ImagePicker Error: ', response.error);
+            }
+            else if (response.customButton) {
+              console.log('User tapped custom button: ', response.customButton);
+            }
+            else {
+              let source = { uri: response.uri };
+              this.setState({
+                avatarSource: source
+              });
+            }
+          });
+    }
 
     render() {
 
         return (
             <View >
+                <TouchableOpacity onPress={() => this.onImagePicker()} >
                 <Text> Ho Lam Lai</Text>
+                </TouchableOpacity>
+                <Image source={this.state.avatarSource} style={{ width : 100, height : 100 }} />
             </View>
         );
     }
